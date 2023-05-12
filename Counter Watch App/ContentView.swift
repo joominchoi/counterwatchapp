69a58f822c8ec937f4c26c7ee1c6dc59868658af
JooMin Choi <joomin.choi@clearscore.com>
2023-05-12 13:59:09 +0100

    creation of basic counter watch app

--- Counter Watch App/ContentView.swift
+++ Counter Watch App/ContentView.swift
@@ -8,14 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentCount = UserDefaults.standard.integer(forKey: "currentCount")
+    
+    @State private var isDecrementOn = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            VStack(alignment: .center, spacing: 10) {
+                HStack(alignment: .center, spacing: 20) {
+                    Button(action: {
+                        self.isDecrementOn.toggle()
+                    }) {
+                        Image(systemName: isDecrementOn ? "minus" : "plus")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    
+                    Button(action: {
+                        self.currentCount = 0
+                        UserDefaults.standard.set(0, forKey: "currentCount")
+                    }) {
+                        Image(systemName: "trash")
+                            .font(.headline)
+                            .foregroundColor(.red)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                
+                Button(action: {
+                    self.performMaths(isSubtract: self.isDecrementOn, num: 1)
+                }) {
+                    Text("\(currentCount)")
+                        .font(.title)
+                }
+                
+                ScrollView(.horizontal) {
+                    HStack(alignment: .center, spacing: 10) {
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, num: 1)
+                        }) {
+                            Text(isDecrementOn ? "-1" : "+1")
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, num: 2)
+                        }) {
+                            Text(isDecrementOn ? "-2" : "+2")
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, num: 3)
+                        }) {
+                            Text(isDecrementOn ? "-3" : "+3")
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, num: 4)
+                        }) {
+                            Text(isDecrementOn ? "-4" : "+4")
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, num: 5)
+                        }) {
+                            Text(isDecrementOn ? "-5" : "+5")
+                        }
+                        
+                        Button(action: {
+                            self.performMaths(isSubtract: self.isDecrementOn, num: 10)
+                        }) {
+                            Text(isDecrementOn ? "-10" : "+10")
+                        }
+                        
+                    }
+                }
+            }.padding(.all)
         }
-        .padding()
+    }
+    
+    func performMaths(isSubtract: Bool, num: Int) {
+        
+        var result = self.currentCount
+        
+        if isSubtract == true {
+            result -= num
+        } else {
+            result += num
+        }
+        
+        if result >= 0 {
+            currentCount = result
+            UserDefaults.standard.set(currentCount, forKey: "currentCount")
+        }
     }
 }
 
