2d013930f194e5864d19005916a59f352cb7b7aa
JooMin Choi <joomin.choi@clearscore.com>
2023-05-12 13:28:37 +0100

    Initial Commit

---
+++ Counter Watch App/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter Watch App
+//
+//  Created by JooMin Choi on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Counter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
